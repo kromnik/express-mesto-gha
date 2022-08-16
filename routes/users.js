9d83f4b1ece9a1ce0c2cc1dc19ebe3bd1d3088ee
Roki K <kiroutp@yandex.ru>
2022-08-16 21:02:47 +0300

    Проектная работа 13_fx_9

--- routes/users.js
+++ routes/users.js
@@ -13,8 +13,4 @@ usersRouter.post('/users', createUser);
 usersRouter.patch('/users/me', updateProfile);
 usersRouter.patch('/users/me/avatar', updateAvatar);
 
-usersRouter.use((req, res) => {
-  res.status(404).send({ message: 'Страница по указанному маршруту не найдена' });
-});
-
 module.exports = usersRouter;
