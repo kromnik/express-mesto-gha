2df2d264b278497537b3c6e6190b14e5633af09a
Roki K <kiroutp@yandex.ru>
2022-09-12 16:11:14 +0300

    Добавление краш теста_бэкенд

--- app.js
+++ app.js
@@ -43,6 +43,12 @@ app.use(cookieParser());
 
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signup', validateSignUp, createUser);
 app.post('/signin', validateSignIn, login);
 
